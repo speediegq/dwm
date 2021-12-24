6a5b1371c8810fd11e73b965ba5490f4526d805d
speedie <71722170+speediegamer@users.noreply.github.com>
2021-12-24 13:06:57 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
-/* speedie's dwm configuration file
-/* Used with my slstatus config on Gentoo
-/* Make sure to install fontawesome before compiling.
-/* Then compile using "make clean install"
-/* Have a good day!
+/* speedie's Dynamic Window Manager configuration file!
+
+Note that this file uses "nan" and "scr" which are shell scripts.
+You may need my shell scripts (@speediegamer/gentoo-scripts)
+and my .bashrc file (since a few things won't work without it)
 
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -25,16 +25,23 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
+static const char *tags[] = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1,       0,           -1 },
+	/* class       instance    title       tags mask     isfloating   monitor */
+        { "Firefox",   NULL,       NULL,       2,            0,           -1 },
+        { "Alacritty", NULL,	   NULL,       3,            0,           -1 },
+        { "Blender",   NULL,	   NULL,       5,            0,           -1 },
+        { "obs",       NULL,	   NULL,       5,            0,           -1 },
+        { "pcmanfm",   NULL,	   NULL,       4,            0,           -1 },
+        { "notepadqq", NULL,	   NULL,       8,            0,           -1 },
+        { "nan",       NULL,	   NULL,       9,            0,	       	  -1 },
+        { "emerge",    NULL,	   NULL,       7,            0,           -1 },
+        { "lg",        NULL,       NULL,       9,            0,           -1 },
 };
 
 /* layout(s) */
@@ -64,26 +71,32 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *firefox[]  = { "firefox-bin", NULL };
+static const char *scrshot[]  = { "scr", NULL };
+static const char *firefox[]  = { "firefox", NULL };
+static const char *pcmanfm[]  = { "pcmanfm", NULL };
+static const char *nan[]  = { "nan", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-        { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = firefox } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = scrshot } },
+        { MODKEY|ShiftMask,             XK_f,	   spawn,          {.v = pcmanfm } },
+	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = firefox } },
+	{ MODKEY|ShiftMask,             XK_t,	   spawn,          {.v = nan } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+/*	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, */
+/*	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, */
+/*	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, */
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -101,7 +114,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_k,      quit,           {0} },
+/*	{ MODKEY|ShiftMask,             XK_c,      quit,           {0} }, */
 };
 
 /* button definitions */
